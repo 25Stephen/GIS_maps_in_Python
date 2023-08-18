46ef166123b3803788be1b52d16b39cadccc618a
Stephen Asare <95316723+25Stephen@users.noreply.github.com>
2023-08-18 18:47:14 +0000

    gis.py

---
+++ gis.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+from cartopy.io.shapereader import Reader
+from cartopy.feature import ShapelyFeature
+from cartopy.io import shapereader
+lon_formatter = LongitudeFormatter(zero_direction_label=True)
+lat_formatter = LatitudeFormatter()
