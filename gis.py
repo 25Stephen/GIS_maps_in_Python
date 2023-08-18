57f3b28393a90dfd33ae1ac7e3a91f71a3b43585
Stephen Asare <95316723+25Stephen@users.noreply.github.com>
2023-08-18 18:48:47 +0000

    Update gis.py

--- gis.py
+++ gis.py
@@ -10,3 +10,8 @@ from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 fig = plt.figure(figsize=(10,5))
 axes = fig.add_subplot(111, projection = crs.PlateCarree())
 axes.set_extent([-20, 40, 0, 25])
+
+countries = ('Benin','Burkina Faso', 'Cameroon','Chad', 'Niger', 
+             'Ivory Coast', 'Equatorial Guinea', 'The Gambia', 'Ghana', 
+             'Guinea', 'Guinea-Bissau', 'Liberia', 'Mali', 'Mauritania', 
+             'Nigeria', 'Senegal', 'Sierra Leone', 'Togo')
