5a596c783d331bcfd2c4e9a46b7d4816100867b0
Stephen Asare <95316723+25Stephen@users.noreply.github.com>
2023-08-18 18:48:35 +0000

    gis.py

--- gis.py
+++ gis.py
@@ -6,3 +6,7 @@ from cartopy.io import shapereader
 lon_formatter = LongitudeFormatter(zero_direction_label=True)
 lat_formatter = LatitudeFormatter()
 from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+
+fig = plt.figure(figsize=(10,5))
+axes = fig.add_subplot(111, projection = crs.PlateCarree())
+axes.set_extent([-20, 40, 0, 25])
