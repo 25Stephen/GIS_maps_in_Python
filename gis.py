aab8ab8463fd5181f05f8076d90a420088451406
Stephen Asare <95316723+25Stephen@users.noreply.github.com>
2023-08-18 18:47:35 +0000

    gis.py

--- gis.py
+++ gis.py
@@ -4,3 +4,4 @@ from cartopy.feature import ShapelyFeature
 from cartopy.io import shapereader
 lon_formatter = LongitudeFormatter(zero_direction_label=True)
 lat_formatter = LatitudeFormatter()
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
